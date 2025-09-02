7cf64d282428ecef044a9c4ca3f392eee88fa9f0
NotAlex575 <dapontealessandro89gmail.com>
2025-09-02 19:49:41 +0200

    aggiorata props cardPersonaggio con il contenuto dell'array di oggetti presenti nell'axios.get

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -8,26 +8,22 @@ const Main = () => {
 
     const fetchPersonaggi = () => {
         axios.get("https://lanciweb.github.io/demo/api/actors/").then((resp) => {
-            console.log(resp);
+            //console.log(resp);  //MILESTONE 1
+            setPersonaggi(resp.data)
         })
     }
+
+    useEffect(() => {
+        fetchPersonaggi();
+    }, []);
 
     return(
         <main>
             <div className="container">
                 <div className="row">
-                    <CardPersonaggio></CardPersonaggio>
-                    <CardPersonaggio></CardPersonaggio>
-                    <CardPersonaggio></CardPersonaggio>
-                    <CardPersonaggio></CardPersonaggio>
-                    <CardPersonaggio></CardPersonaggio>
-                    <CardPersonaggio></CardPersonaggio>
-                    <CardPersonaggio></CardPersonaggio>
-                    <CardPersonaggio></CardPersonaggio>
-                    <CardPersonaggio></CardPersonaggio>
-                    <CardPersonaggio></CardPersonaggio>
-                    <CardPersonaggio></CardPersonaggio>
-                    <CardPersonaggio></CardPersonaggio>
+                    {personaggi.map((personaggio) =>{
+                        return <CardPersonaggio key={personaggio.id} personaggio={personaggio}></CardPersonaggio>
+                    })}
                 </div>
             </div>
             <br /><br />
--- src/assets/cardPersonaggio.jsx
+++ src/assets/cardPersonaggio.jsx
@@ -1,16 +1,17 @@
-const CardPersonaggio = () =>{
-    return(
+const CardPersonaggio = ({ personaggio }) => {
+    const { name, image, birth_year, nationality, biography, known_for, awards } = personaggio;
+    return (
         <div className="col-4 bg-darkBlue">
-            <h2 className="color-white margin-bootm-10px">Nome</h2>
-            <img src="../public/vite.svg" className="margin-bootm-10px" alt="" />
-            <div className="row color-white margin-bootm-10px">
-                <p>anno di nascita</p>
-                <p>nazionalità</p>
+            <h2 className="color-white margin-bottom-10px">{name}</h2>
+            <img src={image} alt={name} className="margin-bottom-10px" />
+            <div className="row color-white margin-bottom-10px">
+                <p>{birth_year}</p>
+                <p>{nationality}</p>
             </div>
-            <p className="color-white margin-bootm-10px">Biografia</p>
-            <p className="color-red">riconoscimenti</p>
+            <p className="color-white margin-bottom-10px w-80per">{biography}</p>
+            <p className="color-red margin-bottom-10px">{known_for}</p>
+            <p className="color-yellow margin-bottom-10px">{awards}</p>
         </div>
-    )
-}
-
+    );
+};
 export default CardPersonaggio;
