fd6cb0b903ee697a65442c3fb4a4c902868e7f3c
NotAlex575 <dapontealessandro89gmail.com>
2025-09-02 18:48:41 +0200

    eseguita  MILESTONE 1  Al caricamento dell'applicazione, recuperiamo la lista degli attori e delle attrici dalle API e stampiamoli in console.

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -1,6 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+
+
 const Main = () => {
+    const [personaggi, setPersonaggi] = useState([]);
+
+    const fetchPersonaggi = () => {
+        axios.get("https://lanciweb.github.io/demo/api/actors/").then((resp) => {
+            console.log(resp);
+        })
+    }
+
     return(
-        <main></main>
+        <main>
+            <button className="" onClick={fetchPersonaggi}>carica personaggi nella console</button>
+        </main>
     )
 }
 
