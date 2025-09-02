4e555339c333789a1c73d7e703baf02460f5671a
NotAlex575 <dapontealessandro89gmail.com>
2025-09-02 19:53:09 +0200

    dimenticato di pushare cardPersonaggio per il push precedente

--- src/assets/cardPersonaggio.jsx
+++ src/assets/cardPersonaggio.jsx
@@ -2,15 +2,15 @@ const CardPersonaggio = ({ personaggio }) => {
     const { name, image, birth_year, nationality, biography, known_for, awards } = personaggio;
     return (
         <div className="col-4 bg-darkBlue">
-            <h2 className="color-white margin-bottom-10px">{name}</h2>
-            <img src={image} alt={name} className="margin-bottom-10px" />
-            <div className="row color-white margin-bottom-10px">
+            <h2 className="color-white margin-bottom-40px margin-top-40px">{name}</h2>
+            <img src={image} alt={name} className="margin-bottom-40px" />
+            <div className="row color-white margin-bottom-40px">
                 <p>{birth_year}</p>
                 <p>{nationality}</p>
             </div>
-            <p className="color-white margin-bottom-10px w-80per">{biography}</p>
-            <p className="color-red margin-bottom-10px">{known_for}</p>
-            <p className="color-yellow margin-bottom-10px">{awards}</p>
+            <p className="color-white margin-bottom-40px w-80per">{biography}</p>
+            <p className="color-red margin-bottom-40px">{known_for}</p>
+            <p className="color-yellow margin-bottom-40px">{awards}</p>
         </div>
     );
 };
